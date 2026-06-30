88c7a962ac5b3243411b473cefa78070e1edb02d
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-06-30 22:43:34 +0500

    Introduction to JavaScript

--- 03-JavaScript/01-Introduction/introduction.js
+++ 03-JavaScript/01-Introduction/introduction.js
@@ -1,2 +1,6 @@
 console.log("Started JavaScript...")
 console.log("Introduction to JavaScript.")
+let variable1 = 10;
+let variable2 = 20;
+let variable3 = variable1 + variable2;
+console.log(variable3)
