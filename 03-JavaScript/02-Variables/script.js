090a498a302e091625e1cf09b5900a061ef05e28
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-06-30 22:50:42 +0500

    Variables in JS

--- 03-JavaScript/02-Variables/script.js
+++ 03-JavaScript/02-Variables/script.js
@@ -3,3 +3,8 @@ let secondNumber = 20;
 let thirdNumber = 30;
 let answer = firstNumber + secondNumber + thirdNumber;
 console.log(answer);
+
+let userName = 'Muhammad Abdullah';
+const userPassword = "`1./#@#.'/,&$";
+console.log(userName);
+console.log(userPassword);
