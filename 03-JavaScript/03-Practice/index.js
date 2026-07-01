841c841570deefd965681bf00353c8b886f21c20
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-07-01 22:24:17 +0500

    Practicing variables and Datatypes in JS

---
+++ 03-JavaScript/03-Practice/index.js
@@ -0,0 +1,20 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Dummy Bot";
+let botLocation = "Universe";
+let favoriteLanguage = "JavaScript";
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact = `${favoriteLanguage} is the most widely used language for web development.`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} is my favorite programming language.`;
+console.log(codingFact);
+
+codingFact = `${favoriteLanguage} adds interactivity to websites.`;
+console.log(codingFact);
+
+console.log(`It was fun sharing these facts with you. Goodbye! - ${botName} from ${botLocation}.`);c
