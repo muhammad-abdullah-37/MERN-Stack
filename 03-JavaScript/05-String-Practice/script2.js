797ab5076f28396d1996d9d7ea040c29d5f96178
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-07-01 22:45:58 +0500

    Practicing an exercise for the practicing the Strings and methods in JavaScript

---
+++ 03-JavaScript/05-String-Practice/script2.js
@@ -0,0 +1,13 @@
+// Step 1: Create a variable called userInput and assign it the value "   Hello World!   ".
+// // Step 2: Log the string "Original input:" followed by the userInput variable.
+// Step 3: Create a variable called cleanedInput and assign it the result of using the trim() method on userInput.
+// Step 4: Log the string "Result of trimming whitespace from both ends:" followed by the cleanedInput variable.
+// Step 5: Create a variable called trimmedStart and assign it the result of using the trimStart() method on userInput.
+// Step 6: Log the string "After using the trimStart() method, leading spaces removed:" followed by the trimmedStart variable.
+// Step 7: Create a variable called trimmedEnd and assign it the result of using the trimEnd() method on userInput.
+// Step 8: Log the string "After using the trimEnd() method, trailing spaces removed:" followed by the trimmedEnd variable.
+// Step 9: Create a variable called upperCaseInput and assign it the result of using the toUpperCase() method on cleanedInput.
+// Step 10: Log the string "Result of using the toUpperCase() method:" followed by the upperCaseInput variable.
+// Step 11: Create a variable called lowerCaseInput and assign it the result of using the toLowerCase() method on cleanedInput.
+// Step 12: Log the string "Result of using the toLowerCase() method:" followed by the lowerCaseInput variable.
+// Step 13: Create a variable called lowercaseWord and generate its camelCase version by combining slice(), toUpperCase(), and slice() methods. Then log the result.
