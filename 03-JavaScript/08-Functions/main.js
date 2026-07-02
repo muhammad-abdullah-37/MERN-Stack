befe653b5a308240f95c71ba853b7853d8ab3a76
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-07-02 17:56:49 +0500

    Question 2 solved

--- 03-JavaScript/08-Functions/main.js
+++ 03-JavaScript/08-Functions/main.js
@@ -37,3 +37,33 @@ Step-by-step Instructions:
 7. Outside the function, create variables that call getLoanMessage() with different income and credit score values to test each possible outcome.
 8. Log each returned message to the console to verify that the function works correctly.
 */
+
+const minIncomeForDuplex = 60000;
+const minCreditScoreForDuplex = 700;
+
+const minIncomeForCondo = 45000;
+const minCreditScoreForCondo = 680;
+
+const minIncomeForCar = 30000;
+const minCreditScoreForCar = 650;
+
+function getLoanMessage(annualIncome, creditScore) {
+  if(creditScore >= minCreditScoreForDuplex && annualIncome >= minIncomeForDuplex) {
+    return "You qualify for a duplex, condo, and car loan."
+  } else if (annualIncome >= minIncomeForCondo && creditScore >= minCreditScoreForCondo) {
+    return "You qualify for a condo and car loan."
+  } else if (annualIncome >= minIncomeForCar && creditScore >= minCreditScoreForCar) {
+    return "You qualify for a car loan."
+  } else {
+    return "You don't qualify for any loans."
+  }
+}
+
+const duplexLoanMsg = getLoanMessage(85000, 850);
+const condoLoanMsg  = getLoanMessage(65000, 690);
+const carLoanMsg    = getLoanMessage(45000, 660);
+const noLoanMsg     = getLoanMessage(25000, 550);
+console.log(duplexLoanMsg);
+console.log(condoLoanMsg);
+console.log(carLoanMsg);
+console.log(noLoanMsg);
