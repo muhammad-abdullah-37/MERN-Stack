7cce73ae22d20d6997adebbbc3ebffc8dce8d4ec
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-07-02 17:35:49 +0500

    Question solved for Contionals in JavaScript

--- 03-JavaScript/06-Conditionals/script.js
+++ 03-JavaScript/06-Conditionals/script.js
@@ -4,3 +4,24 @@
 // Step 4: Use an if statement to check if isTimmyAGamer is true. If it is, log the message "Timmy loves to play World of Warcraft."
 // Step 5: Create a variable called timmyAge and assign it the value 14.
 // Step 6: Use an if...else statement to check if timmyAge is greater than or equal to 16. If it is, log "Timmy is old enough to drive."; otherwise, log "Timmy is not old enough to drive."
+
+// Step 1: 
+const hasDeveloperJob = true;
+// Step 2: 
+if (hasDeveloperJob) {
+  console.log("Timmy is employed as a developer.");
+}
+// Step 3: 
+const isTimmyAGamer = false;
+// Step 4:
+if (isTimmyAGamer) {
+  console.log("Timmy loves to play World of Warcraft.");
+}
+// Step 5:
+const timmyAge = 14;
+// Step 6:
+if (timmyAge >= 16) {
+  console.log("Timmy is old enough to drive.");
+} else {
+  console.log("Timmy is not old enough to drive.");
+}
