4833b855366e13f78745ad55b1faa93fd1d76ee0
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-07-02 17:34:22 +0500

    Conditionals in JavaScript

---
+++ 03-JavaScript/06-Conditionals/script.js
@@ -0,0 +1,6 @@
+// Step 1: Create a variable called hasDeveloperJob and assign it the value true.
+// Step 2: Use an if statement to check if hasDeveloperJob is true. If it is, log the message "Timmy is employed as a developer."
+// Step 3: Create a variable called isTimmyAGamer and assign it the value false.
+// Step 4: Use an if statement to check if isTimmyAGamer is true. If it is, log the message "Timmy loves to play World of Warcraft."
+// Step 5: Create a variable called timmyAge and assign it the value 14.
+// Step 6: Use an if...else statement to check if timmyAge is greater than or equal to 16. If it is, log "Timmy is old enough to drive."; otherwise, log "Timmy is not old enough to drive."
