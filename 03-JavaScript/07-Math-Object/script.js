26088d1081139ef8023ecdd01060b6fae927ff9d
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-07-02 17:46:29 +0500

    Solving a random fortune picker question with the help of conditionals and Math object

--- 03-JavaScript/07-Math-Object/script.js
+++ 03-JavaScript/07-Math-Object/script.js
@@ -91,3 +91,34 @@ console.log(minNum);
 
 // Step 17: 
 console.log("It was fun learning about the different Math methods with you!");
+
+
+// Define the five fortunes
+let fortune1 = "Your cat will look very cuddly today.";
+let fortune2 = "The weather will be nice tomorrow.";
+let fortune3 = "Be cautious of your new neighbors.";
+let fortune4 = "You will find a new hobby soon.";
+let fortune5 = "It would be wise to avoid the color red today.";
+
+// Generate a random number between 1 and 5 (inclusive)
+let randomNumber = Math.floor(Math.random() * 5) + 1;
+
+
+
+// Select the appropriate fortune
+let selectedFortune;
+
+if (randomNumber === 1) {
+  selectedFortune = fortune1;
+} else if (randomNumber === 2) {
+  selectedFortune = fortune2;
+} else if (randomNumber === 3) {
+  selectedFortune = fortune3;
+} else if (randomNumber === 4) {
+  selectedFortune = fortune4;
+} else if (randomNumber === 5) {
+  selectedFortune = fortune5;
+}
+
+// Output the selected fortune
+console.log(selectedFortune);
