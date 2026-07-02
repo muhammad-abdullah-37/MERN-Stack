5061fd9c62f914e687f71281805fd510452a8efe
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-07-02 17:52:26 +0500

    Solving a question for practicing functions

---
+++ 03-JavaScript/08-Functions/main.js
@@ -0,0 +1,12 @@
+/* Question 1 
+Instructions:
+
+1. Create a function named maskEmail that accepts an email address as a parameter.
+2. Find the position of the "@" symbol to separate the username from the domain.
+3. Keep the first and last characters of the username visible.
+4. Extract the characters between the first and last username characters.
+5. Count the number of extracted characters and replace each of them with an asterisk (*).
+6. Combine the first character, the generated asterisks, the last username character, and the original domain to create the masked email address.
+7. Return the masked email from the function.
+8. Outside the function, declare an email variable, call the maskEmail function with it, and print the returned masked email to the console.
+*/
