b2ff2a6592e8a4f44c937a2aa8c35971b9557e0b
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-07-04 11:27:57 +0500

    A practice problem for practicing Conditionals , functions, Arrays , Objects and Array of Objects

---
+++ 03-JavaScript/13-Array-Of-Objects/script2.js
@@ -0,0 +1,46 @@
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+function updateRecords(records,id,prop,value){
+  if(value === "") {
+      console.log("Second condition======")
+    delete records[id][prop];
+  } else if (prop !== "tracks" && value !== ""){
+      console.log("Third condition======")
+      records[id][prop]= value;
+  } else if (prop === "tracks" && value !== "" && records[id][prop] === undefined) {
+      console.log("Fourth condition+++++++++++++++++++++++++++")
+      records[id][prop] = []
+      records[id][prop].push(value)
+  }else if (prop === "tracks" && value !== "") {
+    records[id].tracks.push(value)
+  }else {
+      return "Value is not empty"
+  }
+
+  return records
+}
+
+updateRecords(recordCollection, 5439, "artist", "ABBA")
+updateRecords(recordCollection, 5439, "tracks", "Take a Chance on Me")
+updateRecords(recordCollection, 2548, "artist", "")
+updateRecords(recordCollection, 1245, "tracks", "Addicted to Love")
+updateRecords(recordCollection, 2468, "tracks", "Free")
+updateRecords(recordCollection, 2548, "tracks", "")
+updateRecords(recordCollection, 1245, "albumTitle", "Riptide")
