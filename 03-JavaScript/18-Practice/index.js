6d3fd3656f87f3a4ee87cdf02e7c9345d3e9b2ee
Abdullah Ahmad <abdullahahmad3744@gmail.com>
2026-07-07 11:40:12 +0500

    Solved a practice problem

---
+++ 03-JavaScript/18-Practice/index.js
@@ -0,0 +1,46 @@
+let contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, property) {
+  for (let index = 0; index < contacts.length; index++) {
+
+    // Check if the contact exists
+    if (contacts[index].firstName === name) {
+
+      // Check if the property exists
+      if (contacts[index][property] !== undefined) {
+        return contacts[index][property];
+      } else {
+        return "No such property";
+      }
+
+    }
+  }
+
+  // No contact found after checking all contacts
+  return "No such contact";
+}
